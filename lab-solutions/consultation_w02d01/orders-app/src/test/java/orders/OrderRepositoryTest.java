9bf24142c6105a80328321dca4708d9fdf7269e1
mrtor <mrtorcsaba@gmail.com>
2023-02-13 15:47:02 +0100

    ora

--- lab-solutions/consultation_w02d01/orders-app/src/test/java/orders/OrderRepositoryTest.java
+++ lab-solutions/consultation_w02d01/orders-app/src/test/java/orders/OrderRepositoryTest.java
@@ -1,9 +1,11 @@
 package orders;
 
+import org.assertj.core.api.ObjectAssert;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mariadb.jdbc.MariaDbDataSource;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,5 +31,23 @@ class OrderRepositoryTest {
         flyway.migrate();
 
         orderRepository = new OrderRepository(dataSource);
+    }
+
+    @Test
+    void testSaveOrder(){
+       long id = orderRepository.saveOrder(new Order("Test1", 20, 20));
+        assertNotEquals(0, id);
+    }
+    @Test
+    void testGetOrders(){
+        orderRepository.saveOrder(new Order("Test1", 20, 200));
+        orderRepository.saveOrder(new Order("Test2", 30, 30));
+        orderRepository.saveOrder(new Order("Test3", 40, 240));
+        orderRepository.saveOrder(new Order("Test4", 50, 500));
+
+        = assertThat(orderRepository.getOrders())
+                .hasSize(4)
+                .extracting(Order::getProductName)
+                .containsExactly("Test1","Test2","Test3","Test4");
     }
 }
--- lab-solutions/consultation_w02d01/orders-app/src/test/java/orders/OrderTest.java
+++ lab-solutions/consultation_w02d01/orders-app/src/test/java/orders/OrderTest.java
@@ -1,7 +1,21 @@
 package orders;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderTest {
+    @Test
+    void testCreateOrder(){
+        Order o = new Order("test1",2,2);
+        Order o2 = new Order(1L,"test2",2,3);
+        assertEquals("test1",o.getProductName());
+        assertEquals(2,o.getProductCount());
+        assertEquals(2,o.getPricePerProduct());
+        assertEquals("test2",o2.getProductName());
+        assertEquals(1L,o2.getId());
+        assertEquals(2,o2.getProductCount());
+    }
+
 
 }
